5ed5de65a010413f1de5f0eac3a995dbd34e9ba2
LpCouti <luanesposito2003@gmail.com>
2024-03-28 17:18:00 -0300

    adições

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('homePage.html')
+    return render_template('index.html')
 
 @app.route('/contato')
 def contato():
