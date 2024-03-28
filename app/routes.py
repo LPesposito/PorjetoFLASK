f545cfddea8f8c643d77d6c462d099e37779fdd0
LpCouti <luanesposito2003@gmail.com>
2024-03-28 20:02:54 -0300

    Melhorias e updates

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,23 @@
 from app import app
 from flask import render_template
+from flask import request
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/index', defaults={'nome':'User'})
+@app.route('/index/<nome>')
+def index(nome):
+    return render_template('index.html',nome=nome)
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['GET'])
+def autenticar():
+    usuario = request.args.get('usuario')
+    senha = request.args.get('senha')
+    return f'usuario:{usuario}\nsenha:{senha}'
