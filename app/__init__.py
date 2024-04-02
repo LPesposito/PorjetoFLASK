5fe69dd7b4766e4c39cb70207f6723373b0f7070
LpCouti <luanesposito2003@gmail.com>
2024-04-02 15:21:28 -0300

    melhorias

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "macacos-me-mordam"
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,11 @@
 from app import app
-from flask import render_template
-from flask import request
+from flask import render_template, request, flash, redirect
 
-@app.route('/')
-@app.route('/index', defaults={"nome":"User"})
-@app.route('/index/<nome>')
-def index(nome):
-    return render_template('index.html', nome = nome)
+@app.route("/")
+@app.route("/index/")
+@app.route("/index/<nome>")
+def index(nome="User"):
+    return render_template('index.html', nome=nome)
 
 @app.route('/contato')
 def contato():
@@ -16,8 +15,12 @@ def contato():
 def login():
     return render_template('login.html')
 
-@app.route('/autenticar', methods=['GET'])
+@app.route('/autenticar', methods=['POST'])
 def autenticar():
-    usuario = request.args.get('usuario')
-    senha = request.args.get('senha')
-    return f'usuario:{usuario} senha:{senha}'
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    if (usuario,senha) == ('admin','12345'):
+        return f'usuario:{usuario} senha:{senha}'
+    else: 
+        flash("Dados Invalidos!")
+        return redirect('/login')
